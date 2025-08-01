664449bc13a74f8a035a30454bea971a8e5221a3
alexandremartinxx <alexandremartinx@hotmail.com>
2025-08-01 15:24:32 -0300

    package class

---
+++ main.py
@@ -0,0 +1,22 @@
+class Package:
+    def __init__(self, width, height, length, mass):
+        self.width = width
+        self.height = height
+        self.length = length
+        self.mass = mass
+
+    def is_bulky(self):
+        volume = self.width * self.height * self.length
+        return volume >= 1_000_000 or max(self.width, self.height, self.length) >= 150
+
+    def is_heavy(self):
+        return self.mass >= 20
+
+    def classify(self):
+        bulky = self.is_bulky()
+        heavy = self.is_heavy()
+        return (
+            "REJECTED" if bulky and heavy else
+            "SPECIAL" if bulky or heavy else
+            "STANDARD"
+        )
